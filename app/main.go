d5e54a8193c3fa791aec3e4c6725d9d08db9d420
Abhishek Mandingi <mabhi256@gmail.com>
2025-10-05 10:34:15 -0400

    #CZ2 (Handle invalid commands)

--- app/main.go
+++ app/main.go
@@ -6,12 +6,14 @@ import (
 	"os"
 )
 
-// Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
-var _ = fmt.Fprint
-
 func main() {
 	fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
+	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading stdin: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println(command[:len(command)-1] + ": command not found")
 }
